ce8519a010f063ceac2ea1da577a0ab820f07cff
jack_roberts2@brown.edu <jrober48@vscode1.oscar.ccv.brown.edu>
2025-10-14 22:22:52 -0400

    sh file to be configured once dataset.py finished

--- preprocess.sh
+++ preprocess.sh
@@ -18,10 +18,5 @@ module load python/3.11
 
 source ~/genomics_env/bin/activate
 
-python dataset.py \
-  --hg19_fa       data/hg19/hg19.fa \
-  --chrom_sizes   data/hg19/hg19.chrom.sizes \
-  --blacklist_bed data/blacklist/hg19-blacklist.v2.bed \
-  --epiphany_h5   data/epiphany/GM12878_X.h5 \
-  --epiphany_y    data/epiphany/GM12878_y.pickle \
-  --out_root      data/siamese_5kb_out
+python dataset.py 
+  
